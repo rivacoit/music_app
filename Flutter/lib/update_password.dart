be3161050249776fd59b81df5e43cf0f6549a3c0
Victoria <71730718+rivacoit@users.noreply.github.com>
2023-12-10 18:16:47 -0800

    confirm password color

--- Flutter/lib/update_password.dart
+++ Flutter/lib/update_password.dart
@@ -1,8 +1,12 @@
 // ignore_for_file: prefer_const_constructors, must_be_immutable, use_build_context_synchronously
 
+//import 'dart:js_interop';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_launcher_icons/xml_templates.dart';
 import 'package:music_app/profile.dart';
+import 'package:music_app/signup.dart';
 import 'package:page_transition/page_transition.dart';
 
 import 'components/textfield.dart';
@@ -17,17 +21,62 @@ class _UpdatePasswordPageState extends State<UpdatePasswordPage> {
   final TextEditingController _emailcontroller = TextEditingController();
   User user = FirebaseAuth.instance.currentUser!;
   void _savepassword() async {
-    if (_emailcontroller.text.trim() == user.email!) {
+    if (_emailcontroller.text.trim() == "") {
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+                title: Text(
+                  "Empty Input",
+                  style: TextStyle(
+                    fontFamily: "Poppins",
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                backgroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(0)),
+                ),
+                content: Text(
+                  "You did not enter an email.",
+                  style: TextStyle(
+                    fontFamily: "Poppins",
+                    color: Colors.white,
+                  ),
+                ),
+                actions: [
+                  ElevatedButton(
+                    onPressed: () => Navigator.pop(context),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll<Color>(
+                        Colors.white,
+                      ),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(0),
+                        ),
+                      ),
+                    ),
+                    child: Text(
+                      "OK",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ));
+    } else if (_emailcontroller.text.trim() == user.email!) {
       await FirebaseAuth.instance
           .sendPasswordResetEmail(email: _emailcontroller.text.trim());
+      Navigator.pushReplacement(
+        context,
+        PageTransition(
+          type: PageTransitionType.leftToRight,
+          child: ProfilePage(),
+        ),
+      );
     }
-    Navigator.pushReplacement(
-      context,
-      PageTransition(
-        type: PageTransitionType.leftToRight,
-        child: ProfilePage(),
-      ),
-    );
   }
 
   @override
@@ -40,7 +89,7 @@ class _UpdatePasswordPageState extends State<UpdatePasswordPage> {
               child: GestureDetector(
                 onTap: _savepassword,
                 child: Text(
-                  "Save",
+                  "Confirm",
                   style: TextStyle(
                     fontFamily: "Poppins",
                     fontSize: 18,
